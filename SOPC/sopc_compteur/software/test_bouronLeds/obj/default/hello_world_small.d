c4c3354c343ac43d491f2924bd78be24f26da4b1
mrconde8 <72274371+mrconde8@users.noreply.github.com>
2020-12-14 00:55:44 +0100

    SopcBoutonLed

---
+++ SOPC/sopc_compteur/software/test_bouronLeds/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../test_bouronLeds_bsp//HAL/inc/sys/alt_stdio.h \
+  ../test_bouronLeds_bsp/system.h ../test_bouronLeds_bsp/linker.h
+
+../test_bouronLeds_bsp//HAL/inc/sys/alt_stdio.h:
+
+../test_bouronLeds_bsp/system.h:
+
+../test_bouronLeds_bsp/linker.h:
