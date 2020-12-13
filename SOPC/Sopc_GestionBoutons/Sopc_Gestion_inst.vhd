1b6e280c98a378b58f638b1dba8564b1dacb4f3c
mrconde8 <72274371+mrconde8@users.noreply.github.com>
2020-12-14 00:51:41 +0100

    SopcGestionBouton

---
+++ SOPC/Sopc_GestionBoutons/Sopc_Gestion_inst.vhd
@@ -0,0 +1,17 @@
+  --Example instantiation for system 'Sopc_Gestion'
+  Sopc_Gestion_inst : Sopc_Gestion
+    port map(
+      ledBabord_from_the_avalon_Gestion_0 => ledBabord_from_the_avalon_Gestion_0,
+      ledSTBY_from_the_avalon_Gestion_0 => ledSTBY_from_the_avalon_Gestion_0,
+      ledTribord_from_the_avalon_Gestion_0 => ledTribord_from_the_avalon_Gestion_0,
+      out_bip_from_the_avalon_Gestion_0 => out_bip_from_the_avalon_Gestion_0,
+      out_port_from_the_leds => out_port_from_the_leds,
+      BP_Babord_to_the_avalon_Gestion_0 => BP_Babord_to_the_avalon_Gestion_0,
+      BP_STBY_to_the_avalon_Gestion_0 => BP_STBY_to_the_avalon_Gestion_0,
+      BP_Tribord_to_the_avalon_Gestion_0 => BP_Tribord_to_the_avalon_Gestion_0,
+      clk_0 => clk_0,
+      in_port_to_the_boutons => in_port_to_the_boutons,
+      reset_n => reset_n
+    );
+
+
